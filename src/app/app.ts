90df1bcea48972bc0f860f2a27990493b4021570
Yuki Takei <yuki@weseek.co.jp>
2015-08-30 04:13:47 +0900

    init

---
+++ src/app/app.ts
@@ -0,0 +1,40 @@
+/// <reference path="../../.tmp/typings/bundle.d.ts" />
+
+import 'angular';
+import 'angular-ui-router';
+
+import 'bootstrap';
+import 'bootstrap/css/bootstrap.css!';
+
+
+class MainController {
+  public greeting: String;
+
+  constructor() {
+    this.greeting = 'Hello! AngularJS!';
+  }
+}
+
+angular.module('app', ['ui.router'])
+  // URL Mappings
+  .config(function ($stateProvider: ng.ui.IStateProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider) {
+    $stateProvider
+      .state('top', {
+        url: '/',
+        templateUrl: 'app/main.html',
+        controller: 'MainController as main'
+      })
+    ;
+
+    $urlRouterProvider.otherwise('/');
+  })
+
+  .controller('MainController',MainController)
+
+  .run(function($log: ng.ILogService) {
+    $log.debug('runBlock end');
+  });
+
+angular.element(document).ready(function () {
+    angular.bootstrap(document, ['app']);
+});
